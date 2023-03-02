883fc8d611d00c8bfe19df5fd4f0e439705c7e07
Alef Ojeda de Oliveira <alef.oliveira@dimensa.com.br>
2023-03-02 15:11:25 -0300

    config replication

--- main-replica/config/replica-script.js
+++ main-replica/config/replica-script.js
@@ -6,9 +6,15 @@
 rs.initiate({
     _id: "rsmongo",
     members: [
-        { _id: 0, host: "mongo_host:30004", priority: 0, votes: 1 },
-        { _id: 1, host: "mongo_rs1:30001", priority: 3, votes: 0 },
-        { _id: 2, host: "mongo_rs2:30002", priority: 0, votes: 1 },
-        { _id: 3, host: "mongo_rs3:30003", priority: 0, votes: 1 },
+        // definir o membro 0 como primário
+        { _id: 0, host: "mongo_host:30004", priority: 1, votes: 1 },
+        // definir os membros 1, 2 e 3 como secundários e não eleitores
+        { _id: 1, host: "mongo_rs1:30001"},
+        { _id: 2, host: "mongo_rs2:30002"},
+        { _id: 3, host: "mongo_rs3:30003"},
     ],
+    settings: {
+      // permitir que os membros repliquem apenas de membros primários
+      chainingAllowed: false,
+    }
 });
