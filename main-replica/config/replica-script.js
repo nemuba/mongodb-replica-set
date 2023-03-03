480dd3510f6dc69207088c1b8db14b63a169de01
Alef Ojeda de Oliveira <alef.oliveira@dimensa.com.br>
2023-03-03 17:12:18 -0300

    config network docker-compose.yml

--- main-replica/config/replica-script.js
+++ main-replica/config/replica-script.js
@@ -4,17 +4,13 @@
 // how to set the member to be primary ?
 
 rs.initiate({
-    _id: "rsmongo",
-    members: [
-        // definir o membro 0 como primário
-        { _id: 0, host: "mongo_host:30004", priority: 1, votes: 1 },
-        // definir os membros 1, 2 e 3 como secundários e não eleitores
-        { _id: 1, host: "mongo_rs1:30001"},
-        { _id: 2, host: "mongo_rs2:30002"},
-        { _id: 3, host: "mongo_rs3:30003"},
-    ],
-    settings: {
-      // permitir que os membros repliquem apenas de membros primários
-      chainingAllowed: false,
-    }
+  _id: "rsmongo",
+  members: [
+    // definir o membro 0 como primário
+    { _id: 0, host: "mongo_host:27017", priority: 1, votes: 1 },
+    // definir os membros 1, 2 e 3 como secundários e não eleitores
+    { _id: 1, host: "mongo_rs1:30001" },
+    { _id: 2, host: "mongo_rs2:30002" },
+    { _id: 3, host: "mongo_rs3:30003" },
+  ],
 });
